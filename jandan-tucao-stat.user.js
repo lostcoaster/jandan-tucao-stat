2fc70c9b72c90226407c3c985f292c96655101d0
lostcoaster <fjy456@gmail.com>
2017-07-22 11:30:58 +0900

    use opaque background for quotations

--- jandan-tucao-stat.user.js
+++ jandan-tucao-stat.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Jandan Tucao Stat
 // @namespace    http://github.com/lostcoaster/
-// @version      0.3.1
+// @version      0.3.2
 // @description  Augment jandan tucao system
 // @author       lc
 // @match        http://jandan.net/pic*
@@ -188,7 +188,7 @@ $(function() {
     };
 
     var quotation = {
-        elem: $('<div class="tustat-quote" style="position:fixed;border: 2px solid darkorange;border-radius: 10px;"/>'),
+        elem: $('<div class="tustat-quote" style="position:fixed;border: 2px solid darkorange;border-radius: 10px;background:white;"/>'),
         show: function(cid, x, y){
             var text = $('a[name="'+cid+'"]').parents('.tucao-row').find('.tucao-content').text();
             this.elem.text(text);
